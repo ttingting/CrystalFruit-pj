30a38b268229e42b7ae15b869eeb34284ee2c8fe
Kyle Baik <httpbdz012@gmail.com>
2016-05-16 17:41:37 -0400

    no more .html thing

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/index.html', function (req, res) {
+app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
 
 
-app.get('/contact.html', function (req, res) {
+app.get('/contact', function (req, res) {
    res.sendFile( __dirname + "/" + "contact.html" );
 })
 
